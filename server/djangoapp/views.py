63d8ff985a5784015622e326830db7a55be469bb
dev-pixel9 <dev@pixel9.in>
2026-07-04 18:49:30 +0530

    Implement get_cars endpoint and database populate logic

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -63,3 +63,17 @@ def registration(request):
         return JsonResponse({"userName": username, "status": "Authenticated"})
     else:
         return JsonResponse({"userName": username, "error": "Already Registered"})
+
+from .models import CarMake, CarModel
+from .populate import initiate
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
